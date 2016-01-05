87615c4f24ba5e4b30d00ece1fd65ad35d189505
b94209002 <b94209002@gmail.com>
2016-01-05 14:09:06 +0800

    reset P and T in rad_driver.F

--- CODE/RRTMG/RAD_RRTM_CFMIP/rad_driver.f90
+++ CODE/RRTMG/RAD_RRTM_CFMIP/rad_driver.f90
@@ -335,12 +335,10 @@
 ! Fill out 2D arrays needed by RRTMG 
 
     layerP(:, 1:nzm) = spread(pres (:), dim = 1, ncopies = nx) 
-    layerP(:, nzm+1) = spread((2.*pres(nzm)-pres(nzm-1)), dim = 1, ncopies = nx) ! add layer
+    layerP(:, nzm+1) = 0.5*spread(presi(nzm+1), dim = 1, ncopies = nx) ! add layer
 
     interfaceP(:, 1:nzm+1) = spread(presi(:), dim = 1, ncopies = nx) 
-    interfaceP(:, nzm+2) = MAX(1.e-4_kind_rb,2.*layerP(:,nzm+1)-interfaceP(:,nzm+1)) ! near-zero pressure at top of extra laye
-
-   PRINT*,interfaceP(1, nzm+1),layerP(1,nzm+1),interfaceP(1,nzm+2)
+    interfaceP(:, nzm+2) = MIN(1.e-4_kind_rb,0.25*layerP(1,nzm+1)) ! near-zero pressure at top of extra layer
 
 ! Convert hPa to Pa in layer mass calculation (kg/m2) 
     layerMass(:, 1:nzm+1) = &
@@ -357,10 +355,7 @@
     interfaceT(:, 2:nzm+1) = (layerT(:, 1:nzm) + layerT(:, 2:nzm+1)) / 2. 
 
 ! Extrapolate temperature at top from lapse rate within the layer
-    interfaceT(:, nzm+2) =( layerT(:, nzm+1)*(interfaceP(:,nzm+1)-interfaceP(:, nzm+2)) &
-   - interfaceT(:, nzm+1)*(layerP(:,nzm+1)-interfaceP(:, nzm+2)))/(interfaceP(:,nzm+1)-layerP(:,nzm+1))
-
-!interfaceT(1, nzm+1),layerT(1,nzm+1),interfaceT(1, nzm+2)
+    interfaceT(:, nzm+2) = 2.*layerT(:, nzm+1) - interfaceT(:, nzm+1)
 
 ! Use SST as interface temperature of atmosphere at surface.
     interfaceT(:, 1)  = tg(1:nx) !bloss layerT(:, 1)   + (layerT(:, 1)   - interfaceT(:, 2))   
