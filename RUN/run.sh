97834a9adf278daa148634e2692919011e14e19d
b94209002 <b94209002@gmail.com>
2016-02-18 17:02:49 +0800

    temperaray commit for building tw case

--- RUN/run.sh
+++ RUN/run.sh
@@ -3,19 +3,20 @@
 #PBS -o expname.out
 #PBS -e expname.err
 
-export LD_LIBRARY_PATH=/opt/hdf5-intel/lib:/opt/netcdf4-intel/lib:/opt/openmpi-intel/lib:$LD_LIBRARY_PATH
-export PATH=/opt/openmpi-intel/bin:$PATH
 
-NPROCS=`wc -l < $PBS_NODEFILE`
 
 cd $PBS_O_WORKDIR
 
 export EXPHDR_tmp='expname ../../DATA/expname'
 
+source /aracbox/intel/Compiler/bin/ifortvars.sh intel64
+export LD_LIBRARY_PATH=/opt/netCDF/netCDF-4.3.3.1_intel15/lib:/home/cmw/petsc-intel/lib:/opt/hdf5/intel15/lib:$LD_LIBRARY_PATH
+
 date > runtime
 
-/opt/openmpi-intel/bin/mpirun -np total_cores ./vvm < INPUT | tee OUTPUT
+ldd ./vvm |grep found
+
+/aracbox/mpi/openmpi/1.8.4/intel15/x86_64/bin/mpirun -np total_cores ./vvm < INPUT | tee OUTPUT
 
 date >> runtime
-
 
