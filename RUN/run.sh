1ca3a2c0766d603cb8c295f6649f61bb2216cc58
Mu-Hua Chien <chienmuhua@ntu.edu.tw>
2016-07-19 14:31:50 +0800

    debug complete on ALPS Currently Loaded Modulefiles:   1) compiler/intel/2016   3) hdf5/1.8.16/intel-2016   2) mpi/intel/2016/openmpi/1.6.5   4) netcdf/4.4.0/intel-2016_hdf5-1.8.16

--- RUN/run.sh
+++ RUN/run.sh
@@ -1,21 +1,30 @@
-#PBS -l nodes=np_nodes:ppn=np_threads
-#PBS -N expname
-#PBS -o expname.out
-#PBS -e expname.err
+#!/bin/sh
+#BSUB -a openmpi
+#BSUB -n total_cores
+#BSUB -q test
+#BSUB -o expname.out
+#BSUB -e expname.err
+#BSUB -J expname
+#BSUB -R 'span[ptile=np_threads]'
+#BSUB -R "ipathavail==0"
 
-export LD_LIBRARY_PATH=/opt/hdf5-intel/lib:/opt/netcdf4-intel/lib:/opt/openmpi-intel/lib:$LD_LIBRARY_PATH
-export PATH=/opt/openmpi-intel/bin:$PATH
-
-NPROCS=`wc -l < $PBS_NODEFILE`
-
-cd $PBS_O_WORKDIR
+#cd $PBS_O_WORKDIR
 
 export EXPHDR_tmp='expname ../../DATA/expname'
 
+source /pkg/local/Modules/3.2.10/init/sh
+module load compiler/intel/2015
+module load mpi/intel/2015/openmpi/1.8.4
+module load netcdf/4.4.0/intel-2015_hdf5-1.8.16
+module load hdf5/1.8.16/intel-2015
+
+export LD_LIBRARY_PATH=/home/j40mog00/petsc360/lib:$LD_LIBRARY_PATH
+
 date > runtime
 
-/opt/openmpi-intel/bin/mpirun -np total_cores ./vvm < INPUT | tee OUTPUT
+ldd ./vvm |grep found
+
+mpirun.lsf -np $LSB_DJOB_NUMPROC /work/j40mog00/VVM/DATA/expname/vvm < INPUT | tee /work/j40mog00/VVM/DATA/expname/OUTPUT
 
 date >> runtime
-
 
